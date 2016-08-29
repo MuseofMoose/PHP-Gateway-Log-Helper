881cd99c36803faf14f6d18a20590f88cbeea084
Spencer <sdellis@u.rochester.edu>
2016-08-29 01:04:32 -0400

    Adding base code for Param Logger.

---
+++ ParamLogger.php
@@ -0,0 +1,43 @@
+/**
+ * Combines all parameter names and values into a log message. <p>
+ * Parameters named in the $excludedParamsArray(as strings) will be <b>excluded</b>. <p>
+ * Log types are 'debug', 'info', or 'error'.
+ * @param string $logType
+ * @param array $excludedParamsArray (optional)
+ */
+public function CreateParameterLog($logType, $excludedParamsArray = null){
+    if (!is_string($logType)){
+        $this->log->error('$logType must be a String');
+    }
+    if (!is_null($excludedParamsArray) && !is_array($excludedParamsArray)){
+        $this->log->error('$excludedParamsArray must be an Array');
+    }
+    $backtrace = debug_backtrace()[1];
+    $className = $backtrace['class'];
+    $methodName = $backtrace['function'];
+    $args = $backtrace['args'];
+    $reflectionMethod = new \ReflectionMethod($className, $methodName);
+    $message = $reflectionMethod->class . '::' . $reflectionMethod->name;
+    $reflectionParams = $reflectionMethod->getParameters();
+    foreach ($reflectionParams as $index => $param){
+        $reflectionParams[$index] = $param->name;
+    }
+    if (!is_null($excludedParamsArray)){
+        $reflectionParams = array_diff($reflectionParams, $excludedParamsArray);
+    }
+    $message = $message . (count($reflectionParams) === 0 ? ' call without params' : ' call with params :');
+    foreach ($reflectionParams as $index => $param){
+        $message = $message . ' ' . $param . '=' . $args[$index];
+    }
+    switch ($logType){
+        case 'error':
+            $this->log->error($message);
+            break;
+        case 'info':
+            $this->log->info($message);
+            break;
+        default:
+            $this->log->debug($message);
+            break;
+    }
+}
